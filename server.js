37adc9c59176ab315046dbd4abaec8a63665261c
Leart Krasniqi <leart.krasniqi@prosperoware.com>
2022-01-17 19:32:32 +0100

    First commit

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+const multer = require('multer');
 
+const fileStorage = multer.diskStorage({
+  destination : (req,file,cb) => {
+    cb(null,'./files')
+  },
+  filename : (req,file,cb) =>{
+    cb(null, Date.now()+"--"+file.originalname);
+  }
+}
+)
+const upload = multer({fileStorage})
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,7 +21,10 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req,res) => {
+  console.log(req.file);
+  res.json({ name : req.file.originalname, type : req.file.mimetype, size : req.file.size});
+})
 
 
 const port = process.env.PORT || 3000;
